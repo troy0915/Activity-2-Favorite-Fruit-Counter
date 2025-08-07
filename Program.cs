7bb277937ba5aff336cd7efc18a7e626cd14f369
troy0915 <johntroydolom7@gmail.com>
2025-08-07 16:37:14 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_2_Favorite_Fruit_Counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = new string[5];
+            int appleCount = 0;
+
+            Console.WriteLine("Please enter your 5 favorite fruits:");
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                Console.Write($"Fruit {i + 1}: ");
+                fruits[i] = Console.ReadLine().Trim().ToLower();
+            }
+            foreach (string fruit in fruits)
+            {
+                if (fruit == "apple")
+                {
+                    appleCount++;
+                }
+            }
+            Console.WriteLine($"\nThe fruit 'apple' was entered {appleCount} time(s).");
+        }
+    }
+}
+
+
+
+
